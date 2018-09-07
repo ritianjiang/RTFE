5371a86d2f9dc5ae4eb3a4787d5923865df0d3fb
ritianjiang <1haotian@live.cn>
2018-09-08 00:32:27 +0800

    PSTNP for single strand

--- R/Benneat.R
+++ R/Benneat.R
@@ -43,18 +43,39 @@ setMethod("show", "Benneat",
            }
 )
 
-##' A class used to store the benchmark dataset with the same length so called neat.
-##' @title Benneat
 ##'
-##' @slot Seqs A vector contains the sequences
-##' @slot labs A vector contains the labels like "methylated"/"unmethylated"
-##' @slot seq_num A numeric denotes the num of seqlist
-##' @slot lab_num A table contains the num of each Label
-##' @slot seq_length A numerci denotes the length of each sequence
-##' @docType class
+##' @title PSTNPss
+##'
+##' @slot object A Benneat object
+##' @slot lableA character,Label as the POSITIVE label
+##' @slot lableB character,Label as the POSITIVE label
+##' @docType method
 ##' @export
+##' @return A matrix contains ALL sequences' PSTNPss features,each row denotes
+##' a sequence. The Sequence is sorted by its order in Benneat object
 
-setGeneric("PSTNP",function(object,strand) standardGeneric("PSTNP"))
-setMethod("PSTNP","Benneat",function(object,strand){
+setGeneric("PSTNPss",function(object,lableA,lableB) standardGeneric("PSTNPss"))
 
+setMethod("PSTNPss","Benneat",function(object,lableA,lableB){
+  seq_A<-object@Seqs[object@labs == lableA]
+  seq_B<-object@Seqs[object@labs == lableB]
+  Dic<-expand.grid(c("A","T","C","G"),c("A","T","C","G"),c("A","T","C","G"),
+                   stringsAsFactors = F) %>%
+    apply(1,paste,collapse="") %>% sort
+  F_A<-matrix(-1,nrow=64,ncol=object@seq_length-2)
+  F_B<-matrix(-1,nrow=64,ncol=object@seq_length-2)
+  for(i in 1:object@seq_length-2){
+    F_A[,i]<-lapply(seq_A, str_sub,start=i,end=i+2) %>%
+      do.call(what = "rbind") %>%factor(levels = Dic) %>% table / length(seq_A)
+  }
+  for(i in 1:object@seq_length-2){
+    F_B[,i]<-lapply(seq_B, str_sub,start=i,end=i+2) %>%
+      do.call(what = "rbind") %>%factor(levels = Dic) %>% table / length(seq_A)
+  }
+  Z<- F_A - F_B;rownames(Z)<-Dic
+  P<-matrix(-1,nrow=object@seq_num,ncol=ncol(Z))
+  for(i in 1:ncol(Z)){
+    Z[lapply(object@Seqs, str_sub,start=i,end=i+2) %>% do.call(what="rbind"),i]->P[,i]
+  }
+  return(P %>% as.data.frame())
 })
